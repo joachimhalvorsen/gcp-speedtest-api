58ff8305fb717d487a6053963ae0f06326b1f919
Joachim Halvorsen <joh@computas.com>
2018-11-04 14:28:37 +0100

    writing to pubsub

--- src/main/java/com/example/gcpspeedtestapi/SpeedTestController.java
+++ src/main/java/com/example/gcpspeedtestapi/SpeedTestController.java
@@ -1,16 +1,22 @@
 package com.example.gcpspeedtestapi;
 
+import com.google.gson.Gson;
+import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/speedtest")
 public class SpeedTestController {
+    private final PubSubTemplate pubSub;
 
-    public SpeedTestController() {
+    public SpeedTestController(PubSubTemplate pubSub) {
+        this.pubSub = pubSub;
     }
 
     @PostMapping()
-    public String GetHello(@RequestBody TestResult speedTest) {
-        return String.format("Hello %s", speedTest.getUser());
+    public void PostTestResult(@RequestBody TestResult speedTest) {
+          Gson gson = new Gson();
+          String json = gson.toJson(speedTest);
+          this.pubSub.publish("speedtest", json);
     }
 }
